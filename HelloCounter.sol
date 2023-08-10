e2cacf3dea463e17190d5aa8e2687f4d2a2b0bc5
lightdancr <141926123+lightdancr@users.noreply.github.com>
2023-08-10 20:11:17 +0800

    Create HelloCounter.sol

---
+++ HelloCounter.sol
@@ -0,0 +1,15 @@
+// SPDX-Licence-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloCounter {
+unit 256 public count;
+
+constuctor () {
+  count = 0;
+}
+
+function sayHello() public {
+  count++;
+  }
+  }
+  
