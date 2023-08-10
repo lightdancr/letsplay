58b853f3dc1263737c91d770a2087d5ab09ed758
lightdancr <141926123+lightdancr@users.noreply.github.com>
2023-08-10 20:35:28 +0800

    Update HelloCounter.sol

--- HelloCounter.sol
+++ HelloCounter.sol
@@ -1,10 +1,31 @@
-// SPDX-Licence-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract HelloCounter {
-unit 256 public count;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-constuctor () {
+contract HelloToken is ERC20 {
+uint 256 public token price = 100000000000000;  // 0.0001 Ether in Wei
+
+constructor () ERC20("HelloToken", "HELLO") {
+_mint (msg.sender, 1000000 * 10** decimals())); // mint initial supply
+}
+
+function sayHello() pubic payable {
+  require(msg.value >= tokenPrice, "Insufficient Ether sent");
+  _mint(msg.sender, 1 * 10**decimals ()); // Mint 1 token
+}
+
+function withdrawFunds() public {
+  uint 256 balance = address(this).balance;
+  (bool success, ) = msg.sender.call{value: balance} ("");
+  require(success, "Withdrawl failed");
+}
+}
+
+contract HelloCounter {
+  unit 256 public count;
+
+constructor () {
   count = 0;
 }
 
